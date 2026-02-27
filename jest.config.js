78e98a059e4f973aae0621e9dfd22ee2d739b741
khuyentv <khuyentv@khuyentvs-Mac-mini.local>
2026-02-27 16:59:35 +0700

    init

--- jest.config.js
+++ jest.config.js
@@ -6,4 +6,7 @@ module.exports = {
   restoreMocks: true,
   coveragePathIgnorePatterns: ['node_modules', 'src/config', 'src/app.js', 'tests'],
   coverageReporters: ['text', 'lcov', 'clover', 'html'],
+  testTimeout: 10000,
+  forceExit: true,
+  detectOpenHandles: true,
 };
--- tests/integration/docs.test.js
+++ tests/integration/docs.test.js
@@ -1,14 +1,19 @@
 const request = require('supertest');
 const httpStatus = require('http-status');
 const app = require('../../src/app');
-const config = require('../../src/config/config');
+const setupTestDB = require('../utils/setupTestDB');
 
-describe('Auth routes', () => {
+setupTestDB();
+
+describe('Docs routes', () => {
   describe('GET /v1/docs', () => {
     test('should return 404 when running in production', async () => {
-      config.env = 'production';
+      const originalEnv = process.env.NODE_ENV;
+      process.env.NODE_ENV = 'production';
+
       await request(app).get('/v1/docs').send().expect(httpStatus.NOT_FOUND);
-      config.env = process.env.NODE_ENV;
+
+      process.env.NODE_ENV = originalEnv;
     });
   });
 });
